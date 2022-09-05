76bc26074302f3cef312e2f7671d135a0656292e
sajid Khan <sajidkhan320222gmail.com>
2022-09-05 01:33:02 -0700

    use callback function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const geocode = require('./utils/geocode')
 
 //let url = 'http://api.weatherstack.com/current?access_key=4ddf3325b5246dd35ce408475571ba61&query=New%20York'
 /*request({url,json:true},(error, response, body) =>{
@@ -9,8 +10,31 @@ const request = require('request');
  console.log(response.body. current)
 
 });*/
-const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596&units=f'
+// const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596&units=f'
 
-request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+// request({ url: url, json: true }, (error, response) => {
+//   if(error){
+//     console.log('unable to connect to server')
+//   }else if(response.body.error){
+//     console.log('unable to find location')
+//   }
+//   else {
+//     console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+//   }
+// })
+
+
+/*const geoUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2FqaWRraGFuMzIwMjIiLCJhIjoiY2w3a2k1Nm1xMGFwMzNucDNtNnc5NGMxMCJ9.yA-BcgHUhaPDIp6blhCDBw"
+
+request({url: geoUrl, json: true}), (error, response)=>{
+  const latitude = response.body.features[0].center[0]
+  const longitude =  response.body.features[0].center[1]
+  console.log(latitude)
+  console.log(response)
+}*/
+const address = process.argv[2]
+console.log(process.argv)
+geocode(address, (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
 })
