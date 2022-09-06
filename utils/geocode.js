da129a6f3e9dfda607b2945da98031921c35489f
sajid Khan <sajidkhan320222gmail.com>
2022-09-06 01:04:01 -0700

    updated

--- utils/geocode.js
+++ utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic2FqaWRraGFuMzIwMjIiLCJhIjoiY2w3a2k1Nm1xMGFwMzNucDNtNnc5NGMxMCJ9.yA-BcgHUhaPDIp6blhCDBw'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error,response) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (response.body.features.length === 0) {
